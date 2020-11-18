fe76f2f19a552b7dabb8766f78a55fa8dc0eb12c
TanmaySingh0908 <73775244+TanmaySingh0908@users.noreply.github.com>
2020-11-18 10:49:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+
+  fixedRect = createSprite(400,200,50,80);
+  fixedRect.shapeColor = "green"; 
+ movingRect = createSprite(400,700,80,30);
+ movingRect.velocityY = -4;
+ movingRect.shapeColor = "green" ;
+ movingRect.debug = true;
+ fixedRect.debug = true;
+}
+
+function draw() {
+  background(0);  
+ 
+  
+  if (movingRect.x-fixedRect.x < fixedRect.width/2+movingRect.width/2
+    &&fixedRect.x - movingRect.x< fixedRect.width/2 + movingRect.width/2
+   ){
+    
+    movingRect.velocityX = movingRect.velocityX * -1;
+  }
+  if ( movingRect.y - fixedRect.y< fixedRect.height/2+movingRect.height/2
+    &&fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+
+    movingRect.velocityY = movingRect.velocityY * -1;
+    }
+
+
+  drawSprites();
+}
